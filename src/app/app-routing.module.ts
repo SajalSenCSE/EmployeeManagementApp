6db0eac762106438a8a63348b7f0159e9474eab8
SajalSenCSE <sajalsen.cse.diu@gmailo.com>
2022-09-12 10:53:24 +0600

    Salary2DShow

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeDetailsComponent } from './Employee/employee-details/employee-details.component';
 import { EmployeeListComponent } from './Employee/employee-list/employee-list.component';
+import { LeadListComponent } from './lead-list/lead-list.component';
 
 const routes: Routes = [
   { path: 'emplist', component: EmployeeListComponent },
   { path: 'empdetails', component: EmployeeDetailsComponent },
+  { path: 'leadist', component: LeadListComponent },
 ];
 
 @NgModule({

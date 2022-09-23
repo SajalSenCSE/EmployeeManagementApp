a9850ee069316816d02856e8f72472a7c7e43c87
SajalSenCSE <sajalsen.cse.diu@gmailo.com>
2022-09-23 10:37:23 +0600

    CleanCode

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeAddComponent } from './Employee/employee-add/employee-add.component';
-import { EmployeeDetailsComponent } from './Employee/employee-details/employee-details.component';
 import { EmployeeListComponent } from './Employee/employee-list/employee-list.component';
-import { EmployeeUpdateComponent } from './Employee/employee-update/employee-update.component';
 import { LeadListComponent } from './lead-list/lead-list.component';
 
 const routes: Routes = [
   { path: 'emplist', component: EmployeeListComponent },
-  { path: 'empdetails', component: EmployeeDetailsComponent },
   { path: 'add', component: EmployeeAddComponent },
   { path: 'leadist', component: LeadListComponent },
   { path: 'add/:id', component: EmployeeAddComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,43 @@
-
 import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { EmployeeListComponent } from './Employee/employee-list/employee-list.component';
-import { EmployeeDetailsComponent } from './Employee/employee-details/employee-details.component';
 import { EmployeeServiceService } from './services/employee-service.service';
 import { HttpClientModule } from '@angular/common/http';
-import { MatPaginatorModule } from '@angular/material/paginator';
 import { AgePipe } from './pipes/age.pipe';
 import { SortPipe } from './pipes/sort.pipe';
 import { FilterPipe } from './pipes/filter.pipe';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LeadListComponent } from './lead-list/lead-list.component';
-import { PaginationModule,PaginationConfig } from 'ngx-bootstrap/pagination';
+import { PaginationModule, PaginationConfig } from 'ngx-bootstrap/pagination';
 import { EmployeeAddComponent } from './Employee/employee-add/employee-add.component';
-import { EmployeeUpdateComponent } from './Employee/employee-update/employee-update.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     EmployeeListComponent,
-    EmployeeDetailsComponent,
     AgePipe,
     SortPipe,
     FilterPipe,
     LeadListComponent,
     EmployeeAddComponent,
-    EmployeeUpdateComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    PaginationModule, 
-    ReactiveFormsModule
+    PaginationModule,
+    ReactiveFormsModule,
   ],
-  providers: [EmployeeServiceService,PaginationConfig ],
+  providers: [EmployeeServiceService, PaginationConfig],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
